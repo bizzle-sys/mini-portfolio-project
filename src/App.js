10a0c760af9cf3ad6654b3523e88c0c6c92f25b1
Reeamsh Maharjan <reamsmaharjan@hotmail.com>
2023-03-09 09:22:24 +1100

    stylesheet imported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <Button variant="primary">Primary</Button>
+      <Button><i class="fa-solid fa-house"></i>asd</Button>
       Hello
     </div>
   );
